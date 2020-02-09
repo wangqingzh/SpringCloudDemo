13114f6d865abb8647ec3312e3ffd5d6ffe47b6d
wangqingzh <qingwang7962@gmail.com>
2020-02-09 11:25:59 +0800

    eureka-client

--- eureka-security-server/src/main/java/com/wangqing/eurekasecurityserver/EurekaSecurityServerApplication.java
+++ eureka-security-server/src/main/java/com/wangqing/eurekasecurityserver/EurekaSecurityServerApplication.java
@@ -1,13 +1,13 @@
-package org.example.eurekaserver;
+package com.wangqing.eurekasecurityserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+@SpringBootApplication
+public class EurekaSecurityServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(EurekaSecurityServerApplication.class, args);
     }
 }

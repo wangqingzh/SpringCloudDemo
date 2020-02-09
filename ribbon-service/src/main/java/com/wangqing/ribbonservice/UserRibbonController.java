992ff22be5daf4d775a396b4b05fd604e0ef574a
wangqingzh <qingwang7962@gmail.com>
2020-02-09 16:32:24 +0800

    ribbon

---
+++ ribbon-service/src/main/java/com/wangqing/ribbonservice/UserRibbonController.java
@@ -0,0 +1,24 @@
+package com.wangqing.ribbonservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/user")
+public class UserRibbonController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${service-url.user-service}")
+    private String userServiceUrl;
+
+    @GetMapping("/create")
+    public String cteate(){
+        return restTemplate.getForObject(userServiceUrl + "/user/create", String.class);
+    }
+}
